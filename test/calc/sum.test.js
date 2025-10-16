ccc6868cc3a60b901d69bcbca2d3f926798d5895
Cristian Racedo <CristianDRacedo@Hotmail.com>
2025-10-15 22:25:04 -0300

    agregar: pruebas de desarrollo tdd sum.test.js

---
+++ test/calc/sum.test.js
@@ -0,0 +1,24 @@
+import * as chai from "chai";
+import { describe } from "mocha"
+import { sum } from "../../dist/calc/sum.js"
+
+describe('Desarrollo de Funcion sumar', () => {
+    it("Debe sumar dos numeros de cualquier tipo", () => {
+        chai.expect(sum(10,5)).to.equal(15);
+        chai.expect(sum(-10,5)).to.equal(-5);
+        chai.expect(sum(10,-5)).to.equal(5);
+        chai.expect(sum(-10,-5)).to.equal(-15);
+    })
+    it("Debe devolver Error si algun parametro no es numerico",() =>{
+        chai.expect(sum({},5)).to.be.an('object');
+        chai.expect(sum([],5)).to.have.property('message');
+        chai.expect(sum("10",5)).to.be.a('number');
+    })
+    it("Debe devolver Error si no se pasan 2 parametros", () =>{
+        chai.expect(sum(5)).to.have.property('message');
+        chai.expect(sum(5,10,15)).to.have.property('message');
+    })
+    it("Debe devolver 0 si no se pasan parametros", () => {
+        chai.expect(sum()).to.equal(0);
+    })
+})
