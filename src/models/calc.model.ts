54d7502c4dda22a3250ab92b06465e9efff73ca9
Cristian Racedo <CristianDRacedo@Hotmail.com>
2025-10-17 21:19:25 -0300

    agregar: modelo de operaciones calc.model.ts

---
+++ src/models/calc.model.ts
@@ -0,0 +1,8 @@
+import { suma, resta, producto, division } from "../calc/index.ts"
+
+export const calcOp = {
+    suma, adicion: suma,
+    resta, sustraccion: resta,
+    producto, multiplicacion: producto,
+    division, fraccion: division,
+}
