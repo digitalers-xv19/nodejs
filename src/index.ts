ca94ea83b1bea923d201bc1efe6f219eef390b00
Cristian Racedo <CristianDRacedo@Hotmail.com>
2025-10-22 19:43:03 -0300

    editar: actualizar configuracion servidor index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 /** Importaciones */
 import express from "express";
 import { config } from "dotenv"
+
+import { dbConn } from "./utils/dbConn.ts";
 import calcRouter from "./routes/calc.routes.ts";
+import apiRouter from "./routes/api.routes.ts";
 
 /** Configuracion */
 config(); // Variables de Entorno
 const {
     PORT = 3000,
-    URI = 'https://localhost:27017'
-} = process.env
+    URI = 'mongodb://localhost:27017/digitalers'
+} = process.env;
 const app = express(); // Servidor HttpExpress
+dbConn(URI);
 
 /* Middlewares */
 app.use(express.json()) // FormData
@@ -18,5 +22,6 @@ app.use(express.static('public')) // static server
 
 /** Rutas */
 app.use("/api/calc/", calcRouter);
+app.use("/api/", apiRouter);
 /** Ejecucion*/
-app.listen(PORT, () => console.log("service running"))
+app.listen(PORT, () => console.log("service running"));
