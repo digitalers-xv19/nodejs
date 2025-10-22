f8b4de190a15af7b1488fe02dfcd02a41cea23b6
Cristian Racedo <CristianDRacedo@Hotmail.com>
2025-10-22 19:05:39 -0300

    editar: agregar funcion logaritmo src/models/calc.model.ts

--- src/models/calc.model.ts
+++ src/models/calc.model.ts
@@ -1,8 +1,9 @@
-import { suma, resta, producto, division } from "../calc/index.ts"
+import { suma, resta, producto, division, logaritmo } from "../calc/index.ts"
 
 export const calcOp = {
     suma, adicion: suma,
     resta, sustraccion: resta,
     producto, multiplicacion: producto,
     division, fraccion: division,
+    logaritmo, log: logaritmo,
 }
