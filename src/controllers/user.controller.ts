4ae7356c5fded7f8685cd84605a5903c81b0bff6
Cristian Racedo <CristianDRacedo@Hotmail.com>
2025-10-24 19:49:27 -0300

    editar: renderizar respuestas sesion  controllers/ routes/

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,23 +1,37 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import { userModel } from "../models/user.model.ts";
+import { app } from "../models/app.config.ts";
 
 export const addUser = async(req:Request, res:Response) => {
     try{
         const newUser = new userModel(req.body);
         const data = await newUser.save();
-        const response = { message: data ? 'usuario creado exitosamente' : 'no se ha podido crear la cuenta' };
-        return res.json(response);
-    } catch(err){
-        return res.json(err);
+        const response = { message: 'usuario creado exitosamente' };
+        res.render('signup', { app, response });
+    } catch(error){
+        return res.render('signup',{ app, response: {
+            status: true,
+            message: 'no se ha podido registrar la cuenta',
+        }, error });
     }
 }
-export const getUser = async(req:Request, res:Response) => {
+export const getUser = async(req:Request, res:Response, next:NextFunction) => {
     try{
-        const { username, password } = req.body;
-        const results = await userModel.find({username, password});
-        const response = results.length ? results : { message:"no se encontraron usuarios" };
-        return res.json(response);
-    } catch(err){
-        return res.json(err);
+        const { username } = req.body;
+        const results = await userModel.find({username}).limit(1);
+        if (results?.length){ 
+            req.body.account = results[0];
+            return next()
+        }
+        else res.render('login', { app, response: {
+                status: true,
+                message: 'no se encontro la cuenta de usuario'
+            }
+        });
+    } catch(error){
+        return res.render('login', { app, response: {
+            status: true,
+            message: 'no se pudo realizar la accion, ha ocurrido un error'
+        }, error});
     }
 }
--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
 import { checkPwd, encrypt, decrypt } from "../middleware/index.ts"
 import * as auth from "../controllers/user.controller.ts"
+import { staticPage } from "../controllers/static.controller.ts";
+import { app } from "../models/app.config.ts";
 
 const apiRouter = Router();
 
+apiRouter.get('/login', (req, res) => res.render('login'));
+apiRouter.get('/signup', (req, res) => res.render('signup'));
 apiRouter.post('/signup', checkPwd, encrypt, auth.addUser);
-apiRouter.post('/login', encrypt, auth.getUser);
+apiRouter.post('/login', auth.getUser, decrypt);
+apiRouter.get('/home', (req, res) => {
+    console.log(res);
+    res.json(req.params)
+})
+apiRouter.get('{:id}', (req, res) => res.render('index', {
+    app
+}))
 
 export default apiRouter;
