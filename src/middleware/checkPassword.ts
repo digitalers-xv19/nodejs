e2f7cd8b4fff976a00ca23337dcdfa8c54c6bf2d
Cristian Racedo <CristianDRacedo@Hotmail.com>
2025-10-24 19:50:56 -0300

    editar: actualizar comparacion de contraseñas src/middleware/

--- src/middleware/checkPassword.ts
+++ src/middleware/checkPassword.ts
@@ -1,11 +1,14 @@
 import type { NextFunction, Request, Response } from "express";
+import { app } from "../models/app.config.ts";
 
 export const checkPwd = (req:Request, res:Response, next:NextFunction) => {
     const [p1, p2] = req.body.password
     if (p1 === p2) {
-        console.log(req.body)
         req.body.password = p1;
         return next()
     }
-    return res.json({ message: "las contraseñas no coinciden" })
+    return res.render('signup',{ app, response: {
+        status:true, 
+        message: "las contraseñas no coinciden" 
+    }})
 }
--- src/middleware/encrypt.ts
+++ src/middleware/encrypt.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import bcrypt from "bcrypt"
+import { app } from "../models/app.config.ts";
 
 export const encrypt = (req:Request, res:Response, next:NextFunction) => {
     const { password } = req.body
@@ -10,9 +11,13 @@ export const encrypt = (req:Request, res:Response, next:NextFunction) => {
 export const decrypt = (req:Request, res:Response) => {
     const { body: { password, account: {_id, password:hash} }} = req 
     const success = bcrypt.compareSync(password, hash);
-    return res.json({
-        message: success ? 
-        'inicio de sesion exitoso' : 
-        'no se pudieron validar las credenciales'
+    res.render('login', {
+        app,
+        response: {
+            status: !success,
+            message: success ? 
+                'inicio de sesion exitoso' : 
+                'no se pudieron validar las credenciales',
+        }
     })
 }
